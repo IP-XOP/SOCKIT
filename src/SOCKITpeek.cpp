b99e1471f308fcb2269e2f56422e7eb57d3b440e
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2009-06-28 06:46:34 +0000

    added SOCKITpeek.h

---
+++ src/SOCKITpeek.cpp
@@ -0,0 +1,54 @@
+/*
+ *  SOCKITpeek.cpp
+ *  iPeek
+ *
+ *  Created by andrew on 25/04/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+#include "SOCKIT.h"
+#include "SOCKITpeek.h"
+
+int SOCKITpeek(SOCKITpeekStructPtr p){
+	int err = 0;
+
+	Handle dest;
+	SOCKET sockID = 0;
+	extern CurrentConnections *pinstance;
+
+	extern pthread_mutex_t readThreadMutex;
+	pthread_mutex_lock( &readThreadMutex );
+		
+	dest = NewHandle(0);
+
+	if(!p->sockID){
+		err = OH_EXPECTED_NUMBER;
+		goto done;
+	}
+	
+	sockID = (SOCKET)doubleToLong(roundDouble(p->sockID));
+
+	if(pinstance->isSockitOpen(p->sockID,&sockID)){
+		if(pinstance->getWaveBufferInfo(sockID)->readBuffer.getData()){
+			if(err = PtrAndHand((void*)pinstance->getWaveBufferInfo(sockID)->readBuffer.getData(), dest, pinstance->getWaveBufferInfo(sockID)->readBuffer.getMemSize()))
+				goto done;
+			pinstance->getWaveBufferInfo(sockID)->readBuffer.reset();
+			if(pinstance->getWaveBufferInfo(sockID)->toClose){
+				pinstance->closeWorker(sockID);
+			}
+		}
+	} else {
+		err = -1;
+	}
+	
+done:
+	
+	p->dest = dest;
+
+	if(err && dest)
+		DisposeHandle(dest);
+
+	pthread_mutex_unlock( &readThreadMutex );
+	
+	return err;
+};
