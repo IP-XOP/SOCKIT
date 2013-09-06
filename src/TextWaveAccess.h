6b0c9928815c600c1f72dafb9758cd63353f15ae
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2013-09-06 08:22:32 +0000

    fixed point deletion. Was deleting from end, not start

--- src/TextWaveAccess.h
+++ src/TextWaveAccess.h
@@ -12,6 +12,16 @@
 #include <string>
 using namespace std;
 
-//User must make sure that the number of wavepoints (however they are distributed between dimensions)
-//must be equal to the number of tokens. Otherwise an error will be raised.
-int textWaveAccess(waveHndl *textWaveH, vector<string> &tokens, vector<PSInt> tokenSizes, PSInt szTotalTokens);
+/*
+ textWaveAccess - sets a text wave with the contents from a string vector
+ 
+User must make sure that the number of wavepoints (however they are distributed between dimensions)
+must be equal to the number of tokens. Otherwise an error will be raised.*/
+
+int textWaveAccess(waveHndl *textWaveH, vector<string> &tokens);
+
+
+/*
+ put textwave contents into a string vector
+ */
+int textWaveToTokens(waveHndl *textWaveH, vector<string> &tokens);
