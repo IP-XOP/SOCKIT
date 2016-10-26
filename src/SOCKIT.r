2889e9f9f28c75879988f0438dfac46e59ccbca5
Andrew Nelson <andyfaff@gmail.com>
2016-10-27 10:48:54 +1100

    MAINT: updated OSX resource file

--- src/SOCKIT.r
+++ src/SOCKIT.r
@@ -7,15 +7,15 @@ resource 'vers' (1) {						/* XOP version info */
 };
 
 resource 'vers' (2) {						/* Igor version info */
-	0x05, 0x00, release, 0x00, 0,			/* version bytes and country integer */
-	"5.00",
+	0x07, 0x00, release, 0x00, 0,			/* version bytes and country integer */
+	"7.00",
 	"(for Igor Pro 6.00 or later)"
 };
 
 resource 'STR#' (1100) {					/* custom error messages */
 	{
 		/* [1] */
-		"SOCKIT requires Igor Pro 6.00 or later.",
+		"SOCKIT requires Igor Pro 7.00 or later.",
 		/* [2] */
 		"SOCKIT XOP was called to execute an unknown function.",
 		/* [3] */
